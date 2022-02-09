fca71b9ce64cbc0a5b26c0d4768f9b3585dd10fa
guaravzuse <gauravagrawal855@gmail.com>
2022-02-09 17:52:17 +0530

    Working on header component

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -2,7 +2,20 @@ import React from 'react';
 
 import './header.css'
 const Header = () => {
-  return <div>Header</div>;
+  return (
+  <div className='gpt3__header section_padding' id='home'>
+    <div className="gpt3__header-content">
+      <h1 className='gradient__text'>Let's Build Something amazing with GPT-3 OpenAI</h1>
+      <p></p>
+      <div className="gpt3__header-content__input">
+        <input type="email" placeholder='Your Email' name="" id="" />
+        <button type='button'>Get Started</button>
+      </div>
+    </div>
+  </div>
+  
+  
+  );
 };
 
 export default Header;
